be799d363d752c4d15df59585b6e79436ee669dd
Yami-no-karuro <carloborgna98@gmail.com>
2025-03-15 12:18:23 +0100

    Code partialization

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 #include "lib/hash_table.h"
 
 /**
- * Finds the key with the highest occurrence value in the hash table.
- * When all the keys occurs just once the function will return NULL as no most recurring key was found.
+ * Finds the key with the highest occurrence value in the provided hash table.
+ * When all the keys occurs just once the function stall.
  *
  * @param table - The hash table to search in.
  * @param pair - The most recurring pair.
@@ -28,13 +28,15 @@ void compute_most_recurring_pair(HashTable *table, char **pair)
     }
 }
 
-int main() 
+/**
+ * Populates the pairs table.
+ *
+ * @param table - The hash table to populate.
+ * @param input - The input text string.
+ */
+void compute_pairs_table(HashTable *pairs, char *input)
 {
-    HashTable *pairs = ht_create();
-
-    char input[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
     int input_len = strlen(input);
-
     for (int i = 0; i < input_len - 1; i++) {
         char pair[3];
 
@@ -52,12 +54,23 @@ int main()
        
         ht_insert(pairs, pair, pair_count);
     }
+}
 
-    char *most_recurring_pair = NULL;
-    compute_most_recurring_pair(pairs, &most_recurring_pair);
+int main() 
+{
+    char input[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
-    if (NULL != most_recurring_pair)
+    HashTable *pairs = ht_create();
+    while(1) {
+        compute_pairs_table(pairs, input);
+
+        char *most_recurring_pair = NULL;
+        compute_most_recurring_pair(pairs, &most_recurring_pair);
+
         printf("Most recurring pair: %s\n", most_recurring_pair);
+        if (most_recurring_pair == NULL)
+            break;
+    }
 
     ht_free(pairs);
     return 0;
